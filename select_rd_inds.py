8e7608e45184fb4988973e0281fb9c7660ae38ab
nnsnodnb <ahr63_gej@me.com>
2015-09-25 15:32:22 +0900

    Create select function

--- select_rd_inds.py
+++ select_rd_inds.py
@@ -1,11 +1,18 @@
 import numpy as np
 import random as rd
 
-def select(inds , individual_path , individual_order):
+def select(inds , individual_path , individual_order , j , len_dat):
 
-    select_random = [rd.sample(range(inds) , 3) for n in range(3)]
+    select_random = rd.sample(range(inds) , 3)
 
-    path = [individual_path[select_random[n]] for n in range(len(select_random))]
-    order = [individual_order[select_random[n]] for n in range(len(select_random))]
+    # 親個体と被っていないか
+    while j in select_random:
+        select_random = rd.sample(range(inds) , 3)
+
+    tmp_path = [individual_path[n] for n in select_random]
+    tmp_order = [individual_order[n] for n in select_random]
+
+    path = np.array(tmp_path).reshape(3 , len_dat)
+    order = np.array(tmp_order).reshape(3, len_dat)
 
     return path , order
